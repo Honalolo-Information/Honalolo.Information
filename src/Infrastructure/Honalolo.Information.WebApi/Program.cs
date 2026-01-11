e73b0f7c7acf45b3fe2a6c440b5d402dc7da62d3
marcel <lis.marcys@gmail.com>
2026-01-11 16:10:53 +0100

    [chore] added missing packet

--- src/Infrastructure/Honalolo.Information.WebApi/Program.cs
+++ src/Infrastructure/Honalolo.Information.WebApi/Program.cs
@@ -1,3 +1,5 @@
+using Honalolo.Information.Infrastructure;
+
 namespace Honalolo.Information.WebApi
 {
     public class Program
@@ -6,19 +8,23 @@ namespace Honalolo.Information.WebApi
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            builder.Services.AddInfrastructure(builder.Configuration);
+            builder.Services.AddApplication();
 
             builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
-
             app.MapControllers();
 
             app.Run();
