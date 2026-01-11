1ecb1bc167118ebc619d0c6fbb8e6e5ab9195e39
marcel <lis.marcys@gmail.com>
2026-01-11 15:44:38 +0100

    [feat] added missing fields in entities

--- src/Infrastructure/Honalolo.Information.Domain/Entities/Attractions/Food.cs
+++ src/Infrastructure/Honalolo.Information.Domain/Entities/Attractions/Food.cs
@@ -4,6 +4,8 @@ namespace Honalolo.Information.Domain.Entities.Attractions
 {
     public class Food : BaseEntity
     {
+        public int AttractionId { get; set; }
+        public Attraction Attraction { get; set; } = null!;
         public string CuisineType { get; set; } = string.Empty;
     }
 }
--- src/Infrastructure/Honalolo.Information.Domain/Entities/Attractions/Hotel.cs
+++ src/Infrastructure/Honalolo.Information.Domain/Entities/Attractions/Hotel.cs
@@ -4,6 +4,8 @@ namespace Honalolo.Information.Domain.Entities.Attractions
 {
     public class Hotel : BaseEntity
     {
+        public int AttractionId { get; set; } 
+        public Attraction Attraction { get; set; } = null!;
         public string AmenitiesJson { get; set; } = "[]"; // Maps to JSON amenities
     }
 }
