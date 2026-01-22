6861f40cc1621c4bfac5c818564d90f8eece9356
marcel <lis.marcys@gmail.com>
2026-01-22 09:28:05 +0100

    [feat] added new user registration/login/session DTOs

---
+++ src/Infrastructure/Honalolo.Information.Application/DTOs/Users/AuthResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Honalolo.Information.Application.DTOs.Users
+{
+    public class AuthResponseDto
+    {
+        public string Token { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+    }
+}
---
+++ src/Infrastructure/Honalolo.Information.Application/DTOs/Users/RegisterDto.cs
@@ -0,0 +1,12 @@
+﻿using Honalolo.Information.Domain.Enums;
+
+namespace Honalolo.Information.Application.DTOs.Users
+{
+    public class RegisterDto
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public UserRole Role { get; set; } = UserRole.RegisteredUser; // Default
+    }
+}
