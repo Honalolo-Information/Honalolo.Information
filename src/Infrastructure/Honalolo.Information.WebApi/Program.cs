5305d7e55022f4010b32ad1cbcb17f6642a2fcbc
marcel <lis.marcys@gmail.com>
2026-01-22 10:12:40 +0100

    [feat] added JWT handler

--- src/Infrastructure/Honalolo.Information.WebApi/Program.cs
+++ src/Infrastructure/Honalolo.Information.WebApi/Program.cs
@@ -1,5 +1,8 @@
 using Honalolo.Information.Application;
 using Honalolo.Information.Infrastructure;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace Honalolo.Information.WebApi
 {
@@ -11,6 +14,22 @@ namespace Honalolo.Information.WebApi
 
             builder.Services.AddInfrastructure(builder.Configuration);
             builder.Services.AddApplication();
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+                    ValidAudience = builder.Configuration["JwtSettings:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:Key"]!))
+                };
+            });
 
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
@@ -25,7 +44,10 @@ namespace Honalolo.Information.WebApi
             }
 
             app.UseHttpsRedirection();
+
+            app.UseAuthentication();
             app.UseAuthorization();
+
             app.MapControllers();
 
             app.Run();
