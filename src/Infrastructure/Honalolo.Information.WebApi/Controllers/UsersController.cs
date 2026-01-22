d965fac5212d5fb507d4adffbe545c3d94044bf2
marcel <lis.marcys@gmail.com>
2026-01-22 09:34:35 +0100

    [feat] added users controller

---
+++ src/Infrastructure/Honalolo.Information.WebApi/Controllers/UsersController.cs
@@ -0,0 +1,46 @@
+﻿using Honalolo.Information.Application.DTOs.Users;
+using Honalolo.Information.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Honalolo.Information.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public UsersController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<AuthResponseDto>> Register(RegisterDto dto)
+        {
+            try
+            {
+                var result = await _authService.RegisterAsync(dto);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AuthResponseDto>> Login(LoginDto dto)
+        {
+            try
+            {
+                var result = await _authService.LoginAsync(dto);
+                return Ok(result);
+            }
+            catch (Exception ex) // In production, don't expose exception details
+            {
+                return Unauthorized(ex.Message);
+            }
+        }
+    }
+}
