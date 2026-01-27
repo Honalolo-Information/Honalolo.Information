b7ec10da6466304d88f31dfb4128ff6781ee5e70
marcel <lis.marcys@gmail.com>
2026-01-27 18:55:10 +0100

    [feat] added report entity

---
+++ src/Infrastructure/Honalolo.Information.Domain/Entities/Attractions/Report.cs
@@ -0,0 +1,15 @@
+﻿using Honalolo.Information.Domain.Common;
+
+namespace Honalolo.Information.Domain.Entities.Reports
+{
+    public class Report : BaseEntity
+    {
+        public string Title { get; set; } = string.Empty;
+        public DateTime GeneratedAt { get; set; }
+        public int RequestedByUserId { get; set; }
+
+        public string ParametersJson { get; set; } = string.Empty;
+
+        public string DataJson { get; set; } = string.Empty;
+    }
+}
