965802a0860fd328a4eb9bc24d8316c6334ede49
marcel <lis.marcys@gmail.com>
2026-01-28 09:20:15 +0100

    [feat] added user service interface

---
+++ src/Infrastructure/Honalolo.Information.Application/Interfaces/IUserIService.cs
@@ -0,0 +1,11 @@
+﻿using Honalolo.Information.Application.DTOs.Users;
+
+namespace Honalolo.Information.Application.Interfaces
+{
+    public interface IUserService
+    {
+        Task<UserProfileDto> GetProfileAsync(int userId);
+        Task UpdateProfileAsync(int userId, UpdateUserDto dto);
+        Task ChangePasswordAsync(int userId, ChangePasswordDto dto);
+    }
+}
